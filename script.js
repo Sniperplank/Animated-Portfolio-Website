487a25ac2b91fd9dcccd6528be4ea29f44cba0ca
Eilaf Aljundi <elafjundi@gmail.com>
2022-06-17 22:51:40 -0400

    Nav buttons added

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 window.onload = function () {
     removeCurtain();
+    manageButtonsVisibility();
 };
+
+window.setInterval(function () {
+    ;
+}, 1000);
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 async function removeCurtain() {
     await sleep(4500);
@@ -9,6 +18,11 @@ async function removeCurtain() {
     document.getElementById("textOnCurtain").style.zIndex = "-2";
 }
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+async function manageButtonsVisibility() {
+    await sleep(5200);
+    document.getElementById("projectsButton").style.visibility = "visible"
+    await sleep(5200);
+    document.getElementById("aboutButton").style.visibility = "visible"
+    await sleep(5200);
+    document.getElementById("contactButton").style.visibility = "visible"
 }
