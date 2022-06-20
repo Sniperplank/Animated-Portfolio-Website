781ea0196c9b17765b0c060ef0ecd390236f24e8
Eilaf Aljundi <elafjundi@gmail.com>
2022-06-20 04:35:58 -0400

    Home page done?

--- script.js
+++ script.js
@@ -14,8 +14,7 @@ function sleep(ms) {
 async function removeCurtain() {
     await sleep(4500);
     document.getElementById("curtain-0").style.zIndex = "-2";
-    document.getElementById("content").style.zIndex = "1";
-    document.getElementById("textOnCurtain").style.zIndex = "-2";
+    document.getElementById("textOnCurtain-div").style.zIndex = "-2";
 }
 
 async function manageButtonsVisibility() {
